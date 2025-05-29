4215232888faef72bb014597ea7359fb376d3276
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-05-29 10:21:56 +0900

    reformat configure_archlinux.sh
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- configure_archlinux.sh
+++ configure_archlinux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-PKG_CONFIG='i686-pc-linux-gnu-pkg-config' CXX=clang++ cmake -DCMAKE_CXX_FLAGS:STRING='-m32' -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$@"
+PKG_CONFIG='i686-pc-linux-gnu-pkg-config' CXX=clang++ cmake \
+  -DCMAKE_CXX_FLAGS:STRING='-m32' \
+  -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  "$@"
