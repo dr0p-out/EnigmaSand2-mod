f9eaf542bde62403e3d90d755c9b1e612df682ac
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-05-29 10:52:08 +0900

    configure script: check depended pkgs
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- configure_archlinux.sh
+++ configure_archlinux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+pacman -Q binutils gcc gcc-libs gdb \
+  base-devel make pkgconf cmake \
+  llvm clang compiler-rt lld \
+  sdl12-compat sdl_ttf
+
 CXX=clang++ cmake \
   -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
