7fba8f180895769eafe3c3d723b98bdbbe5b7f1e
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-05-29 10:40:52 +0900

    configure script: dont force 32-bit build
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- configure_archlinux.sh
+++ configure_archlinux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PKG_CONFIG='i686-pc-linux-gnu-pkg-config' CXX=clang++ cmake \
-  -DCMAKE_CXX_FLAGS:STRING='-m32' \
+CXX=clang++ cmake \
   -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
