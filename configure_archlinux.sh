2c103e9356e104270f60e25d1abc68d54b8a5074
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-05-29 09:36:05 +0900

    enable more useful debugging compile options
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- configure_archlinux.sh
+++ configure_archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PKG_CONFIG='i686-pc-linux-gnu-pkg-config' cmake -DCMAKE_CXX_FLAGS:STRING=-m32 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$@"
+PKG_CONFIG='i686-pc-linux-gnu-pkg-config' CXX=clang++ cmake -DCMAKE_CXX_FLAGS:STRING='-m32' -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$@"
